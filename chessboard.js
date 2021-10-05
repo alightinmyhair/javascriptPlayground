6e64bc485e34db2b599b38d76dc9e1e01bfb5255
Rachelle Kuhl <alightinmyhair@gmail.com>
2021-10-04 21:15:04 -0500

    Solved Eloquent JavaScript Chessboard problem

--- chessboard.js
+++ chessboard.js
@@ -8,3 +8,47 @@
 //once I have the above complete, refactor so it works for any size
 //define a binding size = 8
 //a grid of the given width and height
+
+//command line 
+
+const size = process.argv[2];
+// const height = process.argv[3];
+
+// const hashtagString = '# # # # \n';
+// const spaceString = ' # # # #\n';
+
+const hashtagString = '# ';
+const spaceString = ' #';
+let _hashtagString = '';
+let _spaceString = '';
+let newLine = '\n';
+
+for (let i = 0; i < size / 2; i++){
+    _hashtagString += hashtagString;
+    _spaceString += spaceString;
+}
+
+_hashtagString += newLine;
+_spaceString += newLine;
+
+let finalString = '';
+
+
+for (let i = 0; i < size; i++) {
+    if ( i % 2 === 0) {
+        finalString += _spaceString;
+    } else {
+        finalString += _hashtagString;
+    }
+}
+
+
+// for (let i = 0; i < size; i++) {
+//     if ( i % 2 == 0) {
+//         finalString += spaceString;
+//     } else {
+//         finalString += hashtagString;
+//     }    
+// }
+
+console.log(finalString);
